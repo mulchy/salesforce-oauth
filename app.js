57bff0ae97c5ca440fbef457ff551810336c8539
Ryan Mulcahy <ryan@primeacademy.io>
2016-08-18 12:17:52 -0500

    Salesforce oauth example

--- app.js
+++ app.js
@@ -1,9 +1,49 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 
-app.get('/*', function(req, res) {
-  console.log(req.params);
-  res.send(200)
+console.log({clientId : process.env.SF_CLIENT_ID,
+  clientSecret : process.env.SF_CLIENT_SECRET,
+  redirectUri : process.env.SF_CALLBACK_URL});
+
+const jsforce = require('jsforce');
+
+//
+// OAuth2 client information can be shared with multiple connections.
+//
+const oauth2 = new jsforce.OAuth2({
+  // you can change loginUrl to connect to sandbox or prerelease env.
+  // loginUrl : 'https://test.salesforce.com',
+  clientId : process.env.SF_CLIENT_ID,
+  clientSecret : process.env.SF_CLIENT_SECRET,
+  redirectUri : process.env.SF_CALLBACK_URL
+});
+
+//
+// Get authz url and redirect to it.
+//
+app.get('/oauth2/auth', function(req, res) {
+  res.redirect(oauth2.getAuthorizationUrl());
+});
+
+//
+// Pass received authz code and get access token
+//
+app.get('/oauth2/callback', function(req, res) {
+  var conn = new jsforce.Connection({ oauth2 : oauth2 });
+  var code = req.param('code');
+  conn.authorize(code, function(err, userInfo) {
+    if (err) { return console.error(err); }
+    // Now you can get the access token, refresh token, and instance URL information.
+    // Save them to establish connection next time.
+    console.log(conn.accessToken);
+    console.log(conn.refreshToken);
+    console.log(conn.instanceUrl);
+    console.log("User ID: " + userInfo.id);
+    console.log("Org ID: " + userInfo.organizationId);
+    // ...
+    res.send('IT WORKS');
+  });
 });
 
 app.listen(process.env.PORT);
